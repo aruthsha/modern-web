225d469bb93d5da59208861e4978f92d58771cff
Aruthsha Induwara Dewasurendra <aruthshadew@gmail.com>
2023-02-21 12:49:38 +0530

    Updated Hero.jsx

--- modern-web/src/components/Hero.jsx
+++ modern-web/src/components/Hero.jsx
@@ -16,15 +16,31 @@ const Hero = () => (
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px]">
             The Next <br className="sm:block hidden" /> {" "}
-            <span className="text-gradient">Generation</span> {" "} <br className="sm:block hidden" />
-            Payment Method
+            <span className="text-gradient">Generation</span>
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
             <GetStarted />
           </div>
-
         </div>
+
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] w-full">
+        Payment Method
+        </h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       </div>
+
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient" />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
+      </div>
+
     </section>
   )
 
